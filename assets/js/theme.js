0b06091e5b8c44379c3cb931a576931b4b8c5b46
Leifu Chen <leifu.chen@gmail.com>
2018-03-31 02:18:42 +0300

    Updated mobile view

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,4 @@
-$("nav a").hover(
+$("nav.nav-menu a").hover(
   function() {
     $(this).find("img.diamond").remove();
     $(this).append($("<img class='diamond ml-2 mb-1' src='assets/images/diamond-green.svg'>"));
@@ -7,3 +7,13 @@ $("nav a").hover(
     $(this).append($("<img class='diamond ml-2 mb-1' src='assets/images/diamond.svg'>"));
   }
 );
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("sidenav").style.height = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("sidenav").style.height = "0";
+}
