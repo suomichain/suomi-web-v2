218a4f387ac053ed3d1ce545be6156500b852f1f
Leifu Chen <leifu.chen@gmail.com>
2018-04-10 02:07:11 +0300

    Added pop-up screen to show demo

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -92,3 +92,12 @@ function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+$(document).ready(function() {
+  $('.popup-link').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    preloader: false,
+    fixedContentPos: false
+  });
+});
