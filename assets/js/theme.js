65b3af10fc501a74229aae359df6a9b35e812970
Leifu Chen <leifu.chen@gmail.com>
2018-07-12 18:45:38 +0300

    Added an announcment

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -108,6 +108,23 @@ $("#mce-EMAIL").keyup(function(event) {
     }
 });
 
+// Get the announment
+var announcement = document.getElementById("announcement");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  announcement.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target != announcement) {
+      announcement.style.display = "none";
+    }
+}
 
 $(document).ready(function () {
     //Disable full page
@@ -115,7 +132,9 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
-    $("body").on("contextmenu",function(e){
-        return false;
-    });
+    // $("body").on("contextmenu",function(e){
+    //     return false;
+    // });
 });
+
+
